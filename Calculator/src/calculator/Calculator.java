52ac0d7d64d03efb716039bb04dbb0f47de5f990
dayCher <75252811+dayCher@users.noreply.github.com>
2022-04-03 22:08:06 -0400

    Add files via upload

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author claussy
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double n1 = 10;
+        double n2 = 5;
+
+        //suma
+        Suma sum = new Suma(n1,n2);
+        sum.mostrarResultado();
+
+        //resta
+        Resta res = new Resta(n1,n2);
+        res.mostrarResultado();
+
+        //multiplicacion
+        Multiplicacion mul = new Multiplicacion(n1,n2);
+        mul.mostrarResultado();
+
+        //division
+        Division div = new Division(n1,n2);
+        div.mostrarResultado();
+    }
+    
+}
---
+++ Calculator/src/calculator/Operacion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author claussy
+ */
+public class Operacion {
+  double n1;
+  double n2;
+  double res;
+  char operacion;
+
+    public void setRes(double res) {
+        this.res = res;
+    }
+
+    
+    public Operacion(double n1, double n2, char operacion) {
+        
+        this.n1 = n1;
+        this.n2 = n2;
+        this.operacion = operacion;
+        
+    }
+    
+        
+    public void mostrarResultado(){
+        
+        System.out.println(this.n1+" "+this.operacion+" "+this.n2+" = "+this.res);
+    }
+}
